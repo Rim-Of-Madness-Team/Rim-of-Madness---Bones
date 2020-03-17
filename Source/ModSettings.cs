b4f26630f0d57b9371b85dd3f5b1490b1c8c7d7b
SihvMan <sihvman1.3.5.3.1@gmail.com>
2020-03-17 00:15:18 -0400

    V1.1 Functional Release
    Initial release version for RW v1.1

---
+++ Source/ModSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace BoneMod
+{
+
+    public class BoneModSettings : ModSettings
+    {
+        public static float boneFactor = 1;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref BoneModSettings.boneFactor, "boneFactor", 0);
+        }
+
+    }
+
+    public class BoneModMod : Mod
+    {
+        BoneModSettings settings;
+
+        public BoneModMod(ModContentPack con) : base(con)
+        {
+            this.settings = GetSettings<BoneModSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            listing.Label("Multiplier");
+            BoneModSettings.boneFactor = listing.Slider(BoneModSettings.boneFactor, 0f, 10f);
+            listing.Label("ROM_SettingsBoneMultiplier_Num".Translate(BoneModSettings.boneFactor));
+            listing.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Rim of Madness - Bones";
+        }
+    }
+}
